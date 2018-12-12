bd92fcba1bf3965a5761ee285b34fd9492e39788
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2018-12-12 01:02:18 -0400

    Use the OT 2.0 API for in our examples.

--- examples/simple.py
+++ examples/simple.py
@@ -1,10 +1,11 @@
 import datetime
 import redis
 
+import opentracing
 import redis_opentracing
 
 # Your OpenTracing-compatible tracer here.
-tracer = None
+tracer = opentracing.Tracer()
 
 if __name__ == '__main__':
     client = redis.StrictRedis()
@@ -12,13 +13,16 @@ if __name__ == '__main__':
     # By default, init_tracing() traces all Redis commands.
     redis_opentracing.init_tracing(tracer)
 
-    # Traced as a SET command.
-    client.set('last_access', datetime.datetime.now())
+    with tracer.start_active_span('main_span'):
 
-    # Traced as a MULTI command with
-    # SET key:00 what
-    # SET foo:01 bar
-    pipe = client.pipeline()
-    pipe.set('key:00', 'what')
-    pipe.set('foo:01', 'bar')
-    print(pipe.execute())
+        # Traced as a SET command,
+        # with main_span as implicit parent.
+        client.set('last_access', datetime.datetime.now())
+
+        # Traced as a MULTI command with
+        # SET key:00 what
+        # SET foo:01 bar
+        pipe = client.pipeline()
+        pipe.set('key:00', 'what')
+        pipe.set('foo:01', 'bar')
+        print(pipe.execute())
